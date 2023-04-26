9cc60e96574a819d6f172fce896696dc604f9997
Michael Laguatan <mila7096@colorado.edu>
2023-04-26 10:35:41 -0600

    Removed secondary platform table

--- allProjectCodeAndComponents/init_data/create.sql
+++ allProjectCodeAndComponents/init_data/create.sql
@@ -11,12 +11,6 @@ CREATE TABLE platforms(
 );
 
 DROP TABLE IF EXISTS videos CASCADE;
-CREATE TABLE platforms(
-    platform_id SERIAL PRIMARY KEY NOT NULL,
-    platform VARCHAR(20)
-);
-
-DROP TABLE IF EXISTS videos CASCADE;
 CREATE TABLE videos(
     video_id SERIAL PRIMARY KEY NOT NULL,
     title text,
